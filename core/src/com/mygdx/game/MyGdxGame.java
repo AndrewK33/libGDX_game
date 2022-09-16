94f58f8d0ca931d0c765beabc5c6c43962b2590a
Andrew <andreykonovalov33rus@gmail.com>
2022-09-16 23:20:16 +0300

    1. Create CharacterAtlasAnimation 2. Draw character animation 3. Create main music

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,40 +2,110 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 	private SpriteBatch batch;
-	private Texture img;
 	private Texture coinSprite;
-	private CoinAnimation coinAnimation;
+	private CharacterAtlasAnimation runRight, runLeft, characterStand, characterState;
+	private Music music;
+	private Sound sound;
+	private MainInputProcessor mainInputProcessor;
+	private float x, y;
+	int dir = 0, step = 1;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		coinSprite = new Texture("coinSprite.png");
-		coinAnimation = new CoinAnimation("coinSprite.png", 1, 6, 15, Animation.PlayMode.LOOP);
+		initProcessor();
+		initAnimation();
+		initMusic();
+		initSound();
 	}
+
+
+	public void initAnimation() {
+		batch = new SpriteBatch();
+		coinSprite = new Texture(Gdx.files.internal("coinSprite.png"));
+		runRight = new CharacterAtlasAnimation("characterAtlas/walkAtlas.atlas", "walk_right", 9, Animation.PlayMode.LOOP);
+		characterStand = new CharacterAtlasAnimation("characterAtlas/walkAtlas.atlas", "stand", 5, Animation.PlayMode.LOOP);
+		characterState = characterStand;
+	}
+	public void initMusic() {
+		music = Gdx.audio.newMusic(Gdx.files.internal("mainTitle.mp3"));
+		music.setPan(0, 0.2f);
+		music.setLooping(true);
+		music.play();
+	}
+	public void initSound() {
+		sound = Gdx.audio.newSound(Gdx.files.internal("hitTheWeapon.wav"));
+	}
+	public void initProcessor() {
+		mainInputProcessor = new MainInputProcessor();
+		Gdx.input.setInputProcessor(mainInputProcessor);
+	}
+
+
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 1, 1, 1);
-		coinAnimation.setTime(Gdx.graphics.getDeltaTime());
-		float x = Gdx.input.getX() - coinAnimation.draw().getRegionWidth()/2;
-		float y = Gdx.graphics.getHeight() - (Gdx.input.getY() + coinAnimation.draw().getRegionHeight()/2);
+		characterState = characterStand;
+		dir = 0;
 
+		if (mainInputProcessor.getOutString().contains("A")) {
+			dir = -1;
+			characterState = runRight;
+		}
+		if (mainInputProcessor.getOutString().contains("D")) {
+			dir = 1;
+			characterState = runRight;
+		}
+		if (mainInputProcessor.getOutString().contains("W")) {
+			y++;
+
+		}
+		if (mainInputProcessor.getOutString().contains("S")) {
+			y--;
+		}
+		if (mainInputProcessor.getOutString().contains("Space")) {
+			x = Gdx.graphics.getWidth()/2;
+			y = Gdx.graphics.getHeight()/2;
+		}
+
+		if (dir == -1) x-=step;
+		if (dir == 1) x+=step;
+
+		TextureRegion tmp = characterState.draw();
+		if (!characterState.draw().isFlipX() & dir == -1) characterState.draw().flip(true, false);
+		if (characterState.draw().isFlipX() & dir == 1) characterState.draw().flip(true, false);
+
+		characterState.setTime(Gdx.graphics.getDeltaTime());
 		batch.begin();
-		batch.draw(coinAnimation.draw(), x, y);
+		batch.draw(characterState.draw(), x, y);
 		batch.end();
+
+
+		if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) sound.play();
+
+
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
 		coinSprite.dispose();
-		coinAnimation.dispose();
+		runRight.dispose();
+		characterState.dispose();
+		characterStand.dispose();
+		music.dispose();
+		sound.dispose();
 	}
+
 }
